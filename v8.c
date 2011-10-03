cbc9737992244a9c1ab623dedea3658f85e10beb
Kersten Burkhardt <kerstenk@gmail.com>
2011-10-03 22:07:00 +0200

    Test

--- v8.c
+++ v8.c
@@ -9,8 +9,6 @@
 #include <vdr/plugin.h>
 #include <v8.h>
 
-using namespace v8;
-
 static const char *VERSION = "0.0.1";
 static const char *DESCRIPTION = "VDR plugins written in JavaScript";
 static const char *MAINMENUENTRY = "V8";
@@ -67,31 +65,6 @@ bool cPluginV8::ProcessArgs(int argc, char *argv[])
 
 bool cPluginV8::Initialize(void)
 {
-	// Create a stack-allocated handle scope.
-	HandleScope handle_scope;
-
-	// Create a new context.
-	Persistent<Context> context = Context::New();
-
-	// Enter the created context for compiling and
-	// running the hello world script.
-	Context::Scope context_scope(context);
-
-	// Create a string containing the JavaScript source code.
-	Handle<String> source = String::New("'Hello' + ', World!'");
-
-	// Compile the source code.
-	Handle<Script> script = Script::Compile(source);
-
-	// Run the script to get the result.
-	Handle<Value> result = script->Run();
-
-	// Dispose the persistent context.
-	context.Dispose();
-
-	// Convert the result to an ASCII string and print it.
-	String::AsciiValue ascii(result);
-	printf("%s\n", *ascii);
 	return true;
 }
 
